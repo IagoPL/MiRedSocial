abc5556d03a24edbdad23bcac8acc9ad09145313
IagoPL <iagoprietolamas@gmail.com>
2022-12-27 18:34:04 +0100

    inicio

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/mi-red-social'),
+      dir: require('path').join(__dirname, './coverage/input-output'),
       subdir: '.',
       reporters: [
         { type: 'html' },
